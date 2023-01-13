cbd00b245dd77fd4c3f562c61f75dec9e2473d78
Cory Boudreaux <cboud31@gmail.com>
2023-01-12 21:42:00 -0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+/*
+1. Build a responsive container for the app.
+*/
+
+function App() {
+  return (
+    <div className="App">
+      <button>Start Quiz</button>
+    </div>
+  );
+}
+
+export default App;
