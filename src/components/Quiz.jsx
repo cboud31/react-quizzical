3a2744618fbd653c39c8ef02da5600e580ec2311
Cory Boudreaux <cboud31@gmail.com>
2023-01-24 19:20:37 -0600

    Clean up Quiz code

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,31 +2,32 @@ import React from "react";
 
 function Quiz(props) {
   const { triviaQuestions } = props;
-  console.log(triviaQuestions);
 
   function shuffle(array) {
     return array.sort(() => Math.random() - 0.5);
   }
 
+  const questionElements = triviaQuestions.map((question) => {
+    const answers = shuffle([
+      question.correct_answer,
+      ...question.incorrect_answers,
+    ]);
+
+    return (
+      <div className="question">
+        <h3>{question.question}</h3>
+        <div className="answer-section">
+          {answers.map((answer) => (
+            <p className="answer">{answer}</p>
+          ))}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div className="quiz-wrapper">
-      {triviaQuestions.map((question) => {
-        const answers = shuffle([
-          question.correct_answer,
-          ...question.incorrect_answers,
-        ]);
-        console.log(answers);
-        return (
-          <div className="question">
-            <h3>{question.question}</h3>
-            <div className="answer-section">
-              {answers.map((answer) => (
-                <p className="answer">{answer}</p>
-              ))}
-            </div>
-          </div>
-        );
-      })}
+      {questionElements}
       <button className="button-check-answers">Check Answers</button>
     </div>
   );
